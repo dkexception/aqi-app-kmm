e23e7c2c034c908ca1ef4211bedace0276df850f
Mangesh Murhe <mangesh.murhe@t-systems.com>
2024-09-23 23:29:33 +0530

    Guide screen UI implemented

---
+++ iosApp/Extensions/FontExt.swift
@@ -0,0 +1,15 @@
+//
+//  FontExt.swift
+//  iosApp
+//
+//  Created by Mangesh Murhe on 23/09/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Font {
+    static let title = Font.system(size: 28, weight: .bold)
+    static let body = Font.system(size:16, weight: .regular)
+    static let body1 = Font.system(size:14, weight: .regular)
+}
--- iosApp/Views/Onboarding/GuideScreen.swift
+++ iosApp/Views/Onboarding/GuideScreen.swift
@@ -2,19 +2,61 @@
 //  GuideScreen.swift
 //  iosApp
 //
-//  Created by Dhanesh Katre on 16/09/24.
+//  Created by Mangesh Murhe on 23/09/24.
 //  Copyright © 2024 orgName. All rights reserved.
 //
 
 import SwiftUI
 
+struct GuidePage {
+    let title: String
+    let imageName: String
+    let description: String
+    let imageAlignment: Alignment
+}
+
 struct GuideScreen: View {
+    let pages = [GuidePage(title: "Breath Better", imageName: "GuideScreenImage", description: "Understand the air around you, wherever you go with the largest coverage of trusted data.",imageAlignment: .leading), GuidePage(title: "Track Pollution", imageName: "GuideScreenImage", description: "Discover your personal exposure during your daily routine and take action to reduce it", imageAlignment: .center), GuidePage(title: "Controll Exposure", imageName: "GuideScreenImage", description: "During your daily routine discover your personal exposure and take action", imageAlignment: .trailing)]
     var body: some View {
-        Text("Guide")
-            .navigationBarBackButtonHidden(true)
+        TabView {
+            ForEach(0..<pages.count, id: \.self) { index in
+                PageView(pageDetails: pages[index])
+            }
+            
+        }.tabViewStyle(.page(indexDisplayMode: .always))
     }
 }
 
+struct PageView: View {
+    let pageDetails: GuidePage
+    
+    var body: some View {
+        VStack(spacing: 32) {
+            HStack {
+                Spacer()
+                Button("Skip") {
+                    
+                }.padding(.trailing, 16)
+            }
+            Image(pageDetails.imageName)
+                .resizable()
+                .frame(height: 315)
+                .scaledToFill()
+                .frame(width: 300, alignment: pageDetails.imageAlignment)
+                .padding(.horizontal, 32)
+    
+            VStack(spacing: 32) {
+                Text(pageDetails.title)
+                    .font(.title)
+                    .multilineTextAlignment(.center)
+                Text(pageDetails.description)
+                    .font(.body1)
+                    .multilineTextAlignment(.center)
+            }.padding(.horizontal, 32)
+            Spacer()
+        }
+    }
+}
 #Preview {
     GuideScreen()
 }
