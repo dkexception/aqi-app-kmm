6fb8725456c1c65e63f08b2d0cf0de3549ac7d99
MangeshMurhe <mangeshmurhe@gmail.com>
2024-09-25 11:47:41 +0530

    Guide screen changes (#1)
    * Guide screen UI implemented
    
    * Guide screen refactored. Button styles added for filled and borderless buttons
    
    ---------
    
    Co-authored-by: Mangesh Murhe <mangesh.murhe@t-systems.com>

---
+++ iosApp/Extensions/FontExt.swift
@@ -0,0 +1,15 @@
+//
+//  FontExt.swift
+//  iosApp
+//
+//  Created by Mangesh Murhe on 23/09/24.
+//  Copyright © 2024 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Font {
+    static let titleF = Font.system(size: 28, weight: .bold)
+    static let bodyF = Font.system(size:16, weight: .regular)
+    static let body1F = Font.system(size:14, weight: .regular)
+}
--- iosApp/Views/Onboarding/GuideScreen.swift
+++ iosApp/Views/Onboarding/GuideScreen.swift
@@ -2,23 +2,120 @@
 //  GuideScreen.swift
 //  iosApp
 //
-//  Created by Dhanesh Katre on 16/09/24.
+//  Created by Mangesh Murhe on 23/09/24.
 //  Copyright © 2024 orgName. All rights reserved.
 //
 
 import SwiftUI
 import Shared
 
+struct GuidePage {
+    let title: String
+    let description: String
+    let imageAlignment: Alignment
+
+}
+
 struct GuideScreen: View {
-    
+    @State var selectedIndex = 0
+    let pagerControlWidth: CGFloat = 40
     let guideViewModel: IGuideViewModel = IOSHelpers().provideGuideViewModel()
+    let pages = [GuidePage(title: "Breath Better", description: "Understand the air around you, wherever you go with the largest coverage of trusted data.", imageAlignment: .leading), GuidePage(title: "Track Pollution", description: "Discover your personal exposure during your daily routine and take action to reduce it", imageAlignment: .center), GuidePage(title: "Controll Exposure", description: "During your daily routine discover your personal exposure and take action", imageAlignment: .trailing)]
+    
     
     var body: some View {
-        
-        Button("Guide Next") {
-            guideViewModel.onEvent(guideEvent: GuideEvent.OnGetStartedClicked())
+        VStack {
+            HStack(){
+                Spacer()
+                Button("Skip") {
+                    guideViewModel.onEvent(guideEvent: GuideEvent.OnSkipClicked())
+                }.buttonStyle(BorderelessButton())
+            }.padding(.trailing, 16)
+            
+            ZStack(alignment:.top) {
+                TabView(selection:$selectedIndex) {
+                    ForEach(0..<pages.count, id: \.self) { index in
+                        PageView(pageDetails: pages[index])
+                    }
+                    
+                }.tabViewStyle(.page(indexDisplayMode: .never))
+                VStack(alignment:.center) {
+                    Spacer()
+                    CustomPager(
+                        totalIndex: pages.count,
+                        selectedIndex: selectedIndex
+                    ).frame(width: pagerControlWidth, height: 6)
+                        .padding(.top, 80)
+                    Spacer()
+                }
+            }
+            
+            Spacer(minLength: 2)
+            
+            Button("Get Started", action: {
+                guideViewModel.onEvent(guideEvent: GuideEvent.OnGetStartedClicked())
+            }).buttonStyle(FilledButton())
+                .disabled(true)
+                .padding(.horizontal, 32)
+            Spacer()
+                .navigationBarBackButtonHidden(true)
         }
-        .navigationBarBackButtonHidden(true)
+    }
+}
+
+struct PageView: View {
+    let pageDetails: GuidePage
+    
+    var body: some View {
+
+        GeometryReader { reader in
+            VStack(spacing: 16) {
+                Image("GuideScreenImage")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: reader.size.width * 0.55, alignment: pageDetails.imageAlignment)
+                    .padding(.horizontal, 32)
+                    .frame(height: 315)
+                Spacer(minLength: 100)
+                Text(pageDetails.title)
+                    .font(.titleF)
+                    .multilineTextAlignment(.center)
+                Text(pageDetails.description)
+                    .font(.body1F)
+                    .multilineTextAlignment(.center)
+                Spacer()
+            }.padding(.horizontal, 32)
+             .background(Color.white)
+            Spacer()
+        }
+    }
+}
+
+struct CustomPager: View {
+    let totalIndex: Int
+    let selectedIndex: Int
+    @Namespace var animation
+    var body: some View {
+        HStack {
+            ForEach(0..<totalIndex, id: \.self) { index in
+                if selectedIndex == index {
+                    Circle()
+                        .fill(.gray.opacity(0.3))
+                        .frame(height: 5)
+                        .overlay(content: {
+                            Circle()
+                                .fill(.blue)
+                                .frame(height: 5)
+                                .matchedGeometryEffect(id: "IndicatorAnimationId", in: animation)
+                        })
+                } else {
+                    Circle()
+                        .fill(.gray.opacity(0.3))
+                        .frame(height: 5)
+                }
+                    
+            }
+        }.animation(.spring, value: UUID())
     }
 }
 
